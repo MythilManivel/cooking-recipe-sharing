80c8dfddff2fdf23f4535a2019a15bc8fab03c65
MythilManivel <m.mythili23manivel@gmail.com>
2025-09-27 22:28:04 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,14 +1,12 @@
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Multer config for handling file uploads
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
@@ -255,4 +253,5 @@ module.exports = {
   extractPublicId,
   getImageMetadata,
   createImageArchive
+
 };
